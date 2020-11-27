8194d9eb030eeb0448937249380f97a9a99d5b73
mikecrbenton <mikecrbenton@hotmail.com>
2020-11-27 17:37:01 -0600

    added database and first migration

--- data/migrations/20201127154255_recipes.js
+++ data/migrations/20201127154255_recipes.js
@@ -1,7 +1,33 @@
 exports.up = async function(knex) {
-
+   await knex.schema.createTable("recipes", (table) => {
+      table.increments("id")
+      table.text("recipe_name").notNull()
+   })
+   await knex.schema.createTable("ingredients", (table) => {
+      table.increments("id")
+      table.text("ingredient_name").notNull()
+      table.float("quantity").notNull()
+   })
+   await knex.schema.createTable("zoos_animals", (table) => {
+      table
+       .integer("recipe_id")
+       .references("id")
+       .inTable("recipes")
+       .onDelete("CASCADE") // DELETES 
+       .onUpdate("CASCADE") // UPDATES REFERENCES
+       .notNull()
+      table
+       .integer("ingredient_id")
+       .references("id")
+       .inTable("ingredients")
+       .onDelete("CASCADE") // DELETES 
+       .onUpdate("CASCADE") // UPDATES REFERENCES
+       .notNull()
+      table.primary("recipe_id")
+   })
 };
 
 exports.down = async function(knex) {
-
+   await knex.schema.dropTableIfExists("ingredients")
+   await knex.schema.dropTableIfExists("recipes")
 };
